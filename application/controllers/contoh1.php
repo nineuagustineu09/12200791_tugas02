e4065600be67af8585f8d156ce8097fd57f0d1ff
nineuagustineu09 <nineuagustineu09@gmail.com>
2021-09-26 12:01:33 +0700

    menambahkan framework

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Nineu Agustineu
+        saya tinggal di daerah Mangkubumi
+        olah raga yang saya sukai adalah 
+        renang nomer tlp 081912778414";
+    }
+}
+?>
